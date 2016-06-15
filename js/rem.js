a1392173a34e42ef103c22c42b1fb3ab756aa8e1
Pengjiaxun <420558221@qq.com>
2016-06-15 11:26:21 +0800

    上传代码

---
+++ js/rem.js
@@ -0,0 +1,14 @@
+//自适应屏幕改变html的font-size大小
+    (function (doc, win) {
+      var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+          var clientWidth = docEl.clientWidth;
+          if (!clientWidth) return;
+          docEl.style.fontSize = 20 * (clientWidth / 375) + 'px';
+        };
+
+      if (!doc.addEventListener) return;
+      win.addEventListener(resizeEvt, recalc, false);
+      doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
